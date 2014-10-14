010deeddb052a2d7aa73a9c3bea409b9b6366eaf
RSamuel@ApolloII <theralphsamuel@gmail.com>
2014-10-14 02:14:58 -0700

    Began Implementing UDP Broadcasting for initial handshake.

--- getfile.js
+++ getfile.js
@@ -1,29 +1,65 @@
 #!/usr/bin/env node
-var netModule = require('net');
-var ip = require('ip');
+var netModule = require('net');  //for streaming socket
+var dgram = require('dgram');   //for broadcast
+var ip = require('ip');  //ip utility
 
 var destination = __dirname;
 var thisIP = ip.address();
 var hostIP = 'localhost';//process.argv[2];
 var PORT = 7657;
+var antennaLife = 10000;
 
+//*************Receive broadcast from server   (udp datagram socket)
+var antennaSocket = dgram.createSocket('udp4');
 
-var socketConnection = new netModule.Socket();
+//begin listening
+antennaSocket.bind(PORT, function listening (){
+  console.log('Listening for host Broadcast...');
 
-socketConnection.connect(PORT, hostIP, function(){
-  //----------Config and handshake-------------------------------
-  //Kill Socket after 5 second inactivity
-  //socketConnection.timeout(5000, function(){socketConnection.destroy();});
-  socketConnection.write('Client at: '+thisIP+' confirms connection.');
-  console.log(socketConnection.localAddress);
-  
-  //--------------Authentication---------------------------
-
-
-  //-----------Receive data and write to file in current directory-------------
-  socketConnection.on('data', function(data){
-    console.log(data.toString());
+  //Configure antenna
+  antennaSocket.on('error', function(err){
+    console.log('Failed to bind to broadcast socket');
+    antennaSocket.close();
+    throw err;
   });
+
+  antennaSocket.on('message', function (msg, envelope){
+    hostIP = envelope.address();
+    console.log('Receiving Broadcast from: ', hostIP);
+    beginTransfer();
+  });
+
+  //stop listening after 10 seconds
+  var killBroadcast = function (){
+    clearInterval(broadcastInterval);
+    console.log('Broadcast Unaswered:' );
+    process.exit(1);
+  };
+
+  setTimeout(killBroadcast, antennaLife);
 
 });
 
+
+
+function beginTransfer() {
+
+  var socketConnection = new netModule.Socket();
+
+  socketConnection.connect(PORT, hostIP, function connected(){
+    //----------Config and handshake-------------------------------
+    //Kill Socket after 5 second inactivity
+    //socketConnection.timeout(5000, function(){socketConnection.destroy();});
+    socketConnection.write('Client at: '+thisIP+' confirms connection.');
+    console.log(socketConnection.localAddress);
+    
+    //--------------Authentication---------------------------
+
+
+    //-----------Receive data and write to file in current directory-------------
+    socketConnection.on('data', function(data){
+      console.log( data.toString() );
+    });
+
+  });
+};
--- sendfile.js
+++ sendfile.js
@@ -1,47 +1,93 @@
 #!/usr/bin/env node
-console.log('the command is working');
-
+var dgram = require('dgram');
 var netModule = require('net');
 var ip = require('ip');
 
 var thisIP = 'localhost';//ip.address();
-var port = 7657;
+var PORT = 7657;
+var broadcastTTL = 2;
 
+//Parse arguments
 var fileToSend = process.argv[2];
 var authentication = process.argv[3];
+
+//debug
 console.log('Server ip: ', ip.address() );
 console.log('local ip: ', thisIP );
 
-var server = netModule.createServer();
 
-server.on('connection', function(socketConnection){
-  //----------Config and handshake-------------------------------
-  //Kill Socket after 5 second inactivity
-  //socketConnection.timeout(5000, function(){socketConnection.destroy();});
-  //LOGS
-  console.log('Server connected to client');
-  socketConnection.write('You have connected to '+thisIP);
-  socketConnection.on('end', function() {
-    console.log('Server disconnected from client.');
+//*************Broadcast intent and location   (udp datagram socket)
+var broadcaster = dgram.createSocket('udp4');
+var broadcastMessage = new Buffer('CLIENT: receiving.');
+var broadcastInterval = 500;
+var broadcastLife = 10000;
+var broadcastTTL = 3;
+
+broadcaster.bind(PORT, function Broadcast(){
+  //Initiate Broadcast
+  var broadcastInterval = setInterval ( function (){
+    broadcaster.send(broadcastMessage, 0, broadcastMessage.length, PORT, function(){});}, 
+    broadcastInterval );
+  //configure broadcast
+  broadcaster.setTTL(broadcastTTL);
+  broadcaster.setBroadcast(1);
+  
+  //Listen for Response and kill broadcast if heard, begin file transfer
+  broadcaster.on('message', function(){
+    stopBroadcast();
+    beginTransfer();
   });
 
-  //--------------Authentication---------------------------
+  //kill broadcast
+  var killBroadcast = function (){
+    clearInterval(broadcastInterval);
+    console.log('Broadcast Unaswered:' );
+    process.exit(1);
+  };
 
-
-
-  //-------Receive client messages--------
-  socketConnection.on('data', function(data){
-    console.log(data.toString());
-  });
+  setTimeout(killBroadcast, broadcastLife);
 
 });
 
 
-//----------------Start Server
-server.listen(port, thisIP, function(){
-  console.log('Server initiated socket. Listening on port: '+port+' at address: '+ thisIP)
-});
 
+
+
+function beginTransfer(){
+
+  //************File Server
+  //-----------------------
+  var server = netModule.createServer();
+
+  //************Socket handler
+  server.on('connection', function(socketConnection){
+    //----------Config and handshake-------------------------------
+    //Kill Socket after 5 second inactivity
+    //socketConnection.timeout(5000, function(){socketConnection.destroy();});
+    //LOGS
+    console.log('Server connected to client');
+    socketConnection.write('You have connected to '+thisIP);
+    socketConnection.on('end', function() {
+      console.log('Server disconnected from client.');
+    });
+
+    //--------------Authentication-------------------
+
+
+    //-------Receive client messages--------
+    socketConnection.on('data', function(data){
+      console.log(data.toString());
+    });
+
+  });
+
+
+  //*******************Start Server
+  server.listen(PORT, thisIP, function(){
+    console.log('Server initiated socket. Listening on PORT: '+PORT+' at address: '+ thisIP)
+  });
+
+}
 
 
 
